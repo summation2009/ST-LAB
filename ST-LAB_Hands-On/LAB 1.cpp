b8724f32a3449ed8b5110556391d696c7673313e
summation2009 <100085999+summation2009@users.noreply.github.com>
2026-09-09 16:49:32 +0700

    upload file

--- ST-LAB_Hands-On/LAB 1.cpp
+++ ST-LAB_Hands-On/LAB 1.cpp
@@ -2,9 +2,11 @@
  LAB01 : Blink LED
 */
 #define LED_PIN 2 // Built-in LED (ESP32) หรือ ESP32 บางรุ่นอยู่ที่ IO 1
+
 void setup() { // ฟังก์ชันเริ่มต้น ทำงานครั้งเดียวเมื่อบอร์ดเริ่มทำงาน
 pinMode(LED_PIN, OUTPUT); // กำหนดโหมดการทำงานของขา GPIO
 } // ขอบเขตบล็อกคำสั่ง
+
 void loop() { // ฟังก์ชันหลัก ทำงานวนซ้ำตลอดเวลา
 digitalWrite(LED_PIN, HIGH); // สั่งให้ขา GPIO ออกค่า HIGH/LOW
 delay(500); // หน่วงเวลา (มิลลิวินาที)
